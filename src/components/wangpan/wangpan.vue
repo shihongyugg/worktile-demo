54f050ff4b53f416eaf6ccfa62622588da1c7aff
yinxiaoxion <1009666319@qq.com>
2018-09-12 20:17:27 +0800

    yxx第一次提交

--- src/components/wangpan/wangpan.vue
+++ src/components/wangpan/wangpan.vue
@@ -1,13 +1,240 @@
 <template>
-    <div>网盘</div>
+   <div class="box">
+       <div class="fl">
+           <div class="header">
+                <span>网盘</span>
+               <input type="text"  class="input"  placeholder="   搜索文件、文件夹"> <i class="iconfont icon-fangdajing i2"></i>
+            </div>
+            <ul>
+                <li>  
+                   <i class="iconfont icon-xiaosanjiaodown-copy" @click="show = !show"></i><router-link to='/wangpan/qywangpan'><i class="iconfont icon-wenjianjia i3"></i>企业网盘</router-link> 
+                </li>
+                 <div v-for="item of list" v-if="show">
+                      <ul class="ul2">
+                          <li @click="routerGo(item.url)"> 
+                              <i class="iconfont icon-wenjianjia i3"></i> {{item.title}}
+                          </li>
+                      </ul>
+                   </div>
+                 <li>
+                    <i class="iconfont icon-xiaosanjiaodown-copy" @click="show1 = !show1"></i><router-link to='/wangpan/gerenwangpan'><i class="iconfont icon-wenjianjia i4"></i>个人网盘</router-link>
+                </li> 
+                <div v-for="item of list2" v-if="show1"> 
+                     <ul class="ul2">
+                          <li @click="routerGo1(item.url)"> 
+                              <i class="iconfont icon-wenjianjia i4"></i>  {{item.title}}
+                          </li>
+                      </ul>
+                </div>
+                 <li >
+                    <i class="iconfont icon-xiaosanjiaodown-copy" ></i> <router-link to=''>成员</router-link>
+                </li> 
+                 <li data-toggle="modal" data-target="#layer">
+                    <i class="iconfont icon-xiaosanjiaodown-copy"></i> <router-link to=''>标签</router-link>
+                </li> 
+                 <li >
+                    <i class="iconfont icon-gongxiang"> </i><router-link to='/wangpan/yuwogongxiang'>与我共享</router-link>
+                </li> 
+                 <li >
+                   <i class="iconfont icon-huishouzhan-copy"></i> <router-link to='/wangpan/huishouzhan'>回收站</router-link>
+                </li> 
+            </ul>
+       </div>
+       <div class="fr">    
+           <router-view> 
+               
+           </router-view>
+       </div>
+       <div role="dialog" id="layer">
+           <div class="modal-header">
+                <i  class="iconfont icon-guanbi" data-dismiss="modal"></i>
+                <p>标签管理</p>  
+                <div>提示：企业全局标签对所有成员均可见，请谨慎添加、编辑和删除标签。</div>
+                <input type="text" placeholder="       搜索标签...">
+                <p class="p2">
+                     <i class="iconfont icon-wenjian i6"></i>
+                    没有标签
+                </p>
+            </div>
+       </div>
+       
+   </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+           list:[
+               {
+                   title:'资料共享',
+                   url: '/wangpan/qywangpan/ziliaogongxiang'    
+               },
+               {
+                   title:'缺陷管理',
+                   url:'/wangpan/qywangpan/quexianguanli'   
+               },
+               {
+                   title:'公司制度',
+                   url:'/wangpan/qywangpan/gongsizhidu'   
+               }
+           ],
+           list2:[
+              {
+                   title:'preson',
+                   url: '/wangpan/gerenwangpan/preson'        
+               } 
+           ],
+            show:false,
+            show1:false 
+        } 
+    },
+     methods:{
+            routerGo(url){
+                //路由跳转
+                this.$router.push({path:url})
+            },
+            routerGo1(url){
+                //路由跳转
+                this.$router.push({path:url})
+            }
+        }
 }
 </script>
 
-<style>
-
+<style lang="css" scoped>
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    .box{
+        width: 94%;
+        height: 100%;
+        background: rgb(238, 238, 238);
+        float: left;
+        border-right: 1px solid #ddd;
+        position: relative;
+        font-size: .875rem;
+    }
+    .fl{
+        width: 240px;
+        height: 100%;
+        background-color: white;
+    }
+    .header span{
+        float: left;
+        margin: 0 20px;
+        color: #333;
+        line-height: 50px;
+        font-size: 16px;
+    }
+    .input{
+        background: #f3f3f3;
+        box-shadow: none;
+        color: #666;
+        height: 36px;
+        font-size: 14px;
+        padding-left: 28px;
+        border-radius: 20px;
+        border: 1px solid transparent;
+        margin: 0 20px;
+        outline: none;
+    }
+    ul{
+        width: 100%;
+    }
+    li{
+        padding: 8px 10px 8px 45px;
+        line-height: 24px;
+        cursor: pointer;
+    }
+    li a{
+        color: #666;  
+    }
+    .router-link-active {
+        background: 0 0;
+    }
+    .fr{
+        width: 81%;
+        height: 100%;
+        background: #f3f3f3;
+        float: right;
+        position: absolute;
+        top: 0;
+        right: 0;
+    }
+    li:hover{
+         box-shadow: 0 0 24px rgba(0,0,0,.18);
+     }
+     i{
+         color: #aaaaaa;
+         font-size: 24px;
+     }
+     .i2{
+        font-size: 20px;
+        position: relative;
+        top: -32px;
+        left: 39px;
+     }
+     .i3{
+        font-size: 14px;
+       color: rgb(34, 215, 187);
+     }
+     .i4{
+        font-size: 14px;
+        color: rgb(249, 105, 170);
+     }
+     .ul2{
+         text-align: center;
+         color: #666;
+     }
+     #layer{
+         display: none;
+     }
+     .modal-header{
+        width: 660px;
+        height: 410px;
+        background-color: #eee;
+        position: absolute;
+        top: 108px;
+        left: 300px;
+        border-radius: 8px;
+    }
+    .modal-header  i{
+        font-size: 16px;
+        float: right;
+        margin: 17px 26px;
+    }
+    .modal-header p{
+       padding: 17px 22px;
+        font-size: 1rem;
+        font-weight: 500;
+        border-bottom: 1px solid #ffffff;
+    }
+    .modal-header div{
+         padding: 17px 22px;
+    }
+    .modal-header input{
+         width: 90%;
+         height: 38px;
+         margin: 0 32px;
+        display: block;
+        line-height: 1.5;
+        color: #333;
+        background-color: #fff;
+        background-clip: padding-box;
+        border: 1px solid #eee;
+        border-radius: .25rem;
+        transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out
+    }
+     .modal-header .p2{
+        text-align: center;
+        border: none;
+           color: #cacaca;
+    }
+    .modal-header .p2 .i6{
+        display: block;
+        font-size: 5rem;
+        margin: 35px 265px;
+    }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,16 @@ import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
 
-// import Anquan from '../components/home/anquan/anquan'
-// import Gaoji from '../components/home/gaoji/gaoji'
-// import Quanju from '../components/home/quanju/quanju'
-// import Renwu from '../components/home/renwu/renwu'
-// import Xiangmu from '../components/home/xiangmu/xiangmu'
 
 import Xiaoxi from '../components/xiaoxi/xiaoxi'
 import Xiangmu from '../components/xiangmu/xiangmu'
 import Rili from '../components/rili/rili'
 import Wangpan from '../components/wangpan/wangpan'
 import Tongxunlu from '../components/tongxunlu/tongxunlu'
+import wp from './wp'
+
+
+
 
 
 export default new Router({
@@ -31,17 +30,17 @@ export default new Router({
         }, {
             path: '/wangpan',
             component: Wangpan,
-            name: '网盘'
+            name: '网盘',
+            children:wp.wp
         },{
           path: '/tongxunlu',
           name:'通讯录',
           component: Tongxunlu,
-      },{
+      },
+      {
         //如果用户随便输入地址,转到首页
         path: '*',
         redirect: '/xiangmu'
-    }
-
-        
+    }    
     ]
 })
